6e0aaf3e4701a6eeba5e024f46a8cfc41f133ae8
Mitch <mhartweg@student.neumont.edu>
2018-08-11 17:27:31 -0600

    fragment changing implemented but can't run bc dumb error i cant fix

--- app/src/main/java/com/example/mitch/databaseoftasks/MainActivity.java
+++ app/src/main/java/com/example/mitch/databaseoftasks/MainActivity.java
@@ -1,30 +1,37 @@
 package com.example.mitch.databaseoftasks;
 
-import android.app.ListActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends ListActivity {
-
-    ArrayList<String> tasks = new ArrayList<String>();
-    ArrayAdapter<String> adapter;
+public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changeFragment(1);
+    }
+
+    @Override
+    public void changeFragment(int id){
+        Fragment fragment = null;
+        if (id == 1) {
+            fragment = new TaskListFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.currentFragment, fragment);
+            ft.commit();
+        } else if (id == 2) {
+            fragment = new AddFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.currentFragment, fragment);
+            ft.commit();
+        } else if (id == 3) {
+            fragment = new EditFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.currentFragment, fragment);
+            ft.commit();
+        }
     }
 }
--- app/src/main/java/com/example/mitch/databaseoftasks/TaskListFragment.java
+++ app/src/main/java/com/example/mitch/databaseoftasks/TaskListFragment.java
@@ -1,7 +1,9 @@
 package com.example.mitch.databaseoftasks;
 
-import android.app.ListFragment;
+import android.app.Activity;
+import android.support.v4.app.ListFragment;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,20 +13,40 @@ import java.util.ArrayList;
 
 public class TaskListFragment extends ListFragment {
 
-    ArrayList<String> tasks = new ArrayList<String>();
-    ArrayAdapter<String> adapter;
+    private ArrayList<String> tasks = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+    private OnFragmentInteractionListener myListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
         adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, tasks);
         setListAdapter(adapter);
+        FloatingActionButton btn = (FloatingActionButton) getView().findViewById(R.id.addButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myListener.changeFragment(2);
+            }
+        });
 
-        return inflater.inflate(R.layout.fragment_task_list, container, false);
+        return view;
     }
 
-    public void addTask(View view){
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(getActivity());
+        try {
+            myListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " does not implement OnFragmentInteractionListener");
+        }
+    }
 
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        myListener = null;
     }
 }
