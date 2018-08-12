b53f4557f9375cbbf75510bf1b5bdb375e326aab
Mitch <mhartweg@student.neumont.edu>
2018-08-11 21:26:22 -0600

    ¯\_(ツ)_/¯

--- app/src/main/java/com/example/mitch/databaseoftasks/TaskListFragment.java
+++ app/src/main/java/com/example/mitch/databaseoftasks/TaskListFragment.java
@@ -8,25 +8,36 @@ import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class TaskListFragment extends ListFragment {
 
-    private ArrayList<String> tasks = new ArrayList<String>();
+    private ArrayList<String> tasks = new ArrayList<>();
     private ArrayAdapter<String> adapter;
     private OnFragmentInteractionListener myListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //set view to fragment_task_list and inflate
         View view = inflater.inflate(R.layout.fragment_task_list, container, false);
+
+        //create adapter for ListView
         adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, tasks);
+        //set adapter
         setListAdapter(adapter);
+
+        //initialize instance of interface so creating the onClick doesn't break on null
         myListener = (OnFragmentInteractionListener) this.getActivity();
+
+        //Get FAB
         FloatingActionButton btn = view.findViewById(R.id.addButton);
+        //Create onClick to call changeFragment
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,9 +45,29 @@ public class TaskListFragment extends ListFragment {
             }
         });
 
+        //Get listview and set clickListener
+        ListView listview = view.findViewById(android.R.id.list);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+                // get item information
+                Toast.makeText(getActivity().getApplicationContext(), "Position: " + position + " ID: " + id, Toast.LENGTH_LONG).show();
+                // switch fragment
+                myListener.changeFragment(3);
+            }
+        });
+
         return view;
     }
 
+//    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+//        // get item information
+//        Toast.makeText(getActivity().getApplicationContext(), "Position: " + position + " ID: " + id, Toast.LENGTH_LONG).show();
+//        // switch fragment
+//        myListener.changeFragment(3);
+//    }
+
+    // sets myListener to the activity hosting the new fragment
     @Override
     public void onAttachFragment(Fragment childFragment) {
         super.onAttachFragment(childFragment);
@@ -47,6 +78,7 @@ public class TaskListFragment extends ListFragment {
         }
     }
 
+    // clears myListener when fragment is detached
     @Override
     public void onDetach() {
         super.onDetach();
